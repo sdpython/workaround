fe52ecc0dca2e118ea3426789bbebef2c407a293
xavier dupré <xavier.dupre@gmail.com>
2022-01-22 18:33:17 +0100

    Update build_cpu.sh

--- build_cpu.sh
+++ build_cpu.sh
@@ -1,23 +1,19 @@
 echo --aten_op_executor--
 cd ort/torch_cpp_extensions/cpu/aten_op_executor
 
-python setup.py bdist_wheel
-if [ $? -ne 0 ]; then exit $?; fi
+python setup.py bdist_wheel || exit 1
 
 echo --torch_interop_utils--
 cd ../torch_interop_utils
 
-python setup.py bdist_wheel
-if [ $? -ne 0 ]; then exit $?; fi
+python setup.py bdist_wheel || exit 1
 
 echo --COPY--
 cd ../../../..
 
-cp ort/torch_cpp_extensions/cpu/aten_op_executor/dist/*.whl .
-if [ $? -ne 0 ]; then exit $?; fi
+cp ort/torch_cpp_extensions/cpu/aten_op_executor/dist/*.whl . || exit 1
 
-cp ort/torch_cpp_extensions/cpu/torch_interop_utils/dist/*.whl .
-if [ $? -ne 0 ]; then exit $?; fi
+cp ort/torch_cpp_extensions/cpu/torch_interop_utils/dist/*.whl . || exit 1
 
 echo --END--
 cd ..
